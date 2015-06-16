fb96321e681a2de79dcc2ef70ff577c5662e6aa3
Charles Hill <chill@degreesofzero.com>
2015-06-16 11:45:55 +0200

    JS code styling checks will now be run along with unit and integrations tests.

--- gruntFile.js
+++ gruntFile.js
@@ -13,11 +13,27 @@ module.exports = function(grunt) {
 				config: '.jscsrc',
 				requireCurlyBraces: [ 'if', 'for', 'while' ]
 			}
+		},
+		mochaTest: {
+			unit: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/unit/**/*.js']
+			},
+			integration: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/integration/**/*.js']
+			}
 		}
 	};
 
 	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
 	grunt.initConfig(config);
 
+	grunt.registerTask('test', [ 'jscs', 'mochaTest' ]);
 };
