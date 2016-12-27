4d0bd1c9b2a5b748c6890f502b56e0d5f07fe973
Agostino Marzotta <a.marzotta@reply.it>
2016-12-27 18:57:51 +0100

    Table creation and db config cleanup

--- test/config.js
+++ test/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  user: process.env.NODE_ORACLEDB_USER || 'c##security',
+  user          : process.env.NODE_ORACLEDB_USER || "hr",
 
   // Instead of hard coding the password, consider prompting for it,
   // passing it in an environment variable via process.env, or using
   // External Authentication.
-  password: process.env.NODE_ORACLEDB_PASSWORD || 'irisSecurity01',
+  password      : process.env.NODE_ORACLEDB_PASSWORD || "welcome",
 
   // For information on connection strings see:
   // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#connectionstrings
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || '10.26.7.20:1521/tinaba',
+  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl",
 
   // Setting externalAuth is optional.  It defaults to false.  See:
   // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#extauth
-  externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
+  externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
 };
